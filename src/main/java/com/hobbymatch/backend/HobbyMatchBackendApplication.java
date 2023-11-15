50f1f21f2f4219835196177b67c96a57b84eb26d
Bettina Justus <bettina.jus@gmail.com>
2023-11-15 18:20:15 +0100

    initial project setup

---
+++ src/main/java/com/hobbymatch/backend/HobbyMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hobbymatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyMatchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hobbymatch/backend/HobbyMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbymatch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyMatchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
